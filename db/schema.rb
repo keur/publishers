2eab69b6f3672cb7e42ed88090e1f6ad7edc6138
Albert Wang <amwang217@gmail.com>
2020-04-14 10:16:04 -0700

    Updated model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_000623) do
+ActiveRecord::Schema.define(version: 2020_04_14_170244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -619,6 +619,16 @@ ActiveRecord::Schema.define(version: 2020_04_14_000623) do
     t.jsonb "stats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "wallet_provider_associations", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.text "wallet_provider_id", null: false
+    t.uuid "publisher_id", null: false
+    t.boolean "denied", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["publisher_id"], name: "index_wallet_provider_associations_on_publisher_id"
+    t.index ["wallet_provider_id"], name: "index_wallet_provider_associations_on_wallet_provider_id"
   end
 
   create_table "youtube_channel_details", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
